e6bd15fcb801dc55c9b7db8c298ab88459106b99
Ezekiel <iamwebwiz@gmail.com>
2019-05-12 15:26:23 +0100

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,40 +10,40 @@ class UserController extends Controller
 {
     public function register(Request $request)
     {
-    	$validator = validator($request->all(), [
-    		'name' => 'required|string|max:255',
-    		'email' => 'required|string|email|max:255',
-    		'password' => 'required|string|max:255|confirmed'
-    	]);
+        $validator = validator($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|max:255|confirmed',
+        ]);
 
-    	if ($validator->fails()) {
-    		return response()->json([
-    			'errors' => $validator->errors(),
-    		], 401);
-    	}
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+            ], 401);
+        }
 
-    	$data = $request->only(['name', 'email', 'password']);
-    	$data['password'] = bcrypt($data['password']);
+        $data = $request->only(['name', 'email', 'password']);
+        $data['password'] = bcrypt($data['password']);
 
-    	$user = User::create($data);
+        $user = User::create($data);
 
-    	return response()->json([
-    		'user' => $user,
-    		'token' => $user->createToken('laraspa')->accessToken,
-    	], 201);
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('laraspa')->accessToken,
+        ], 201);
     }
 
     public function login(Request $request)
     {
-    	if (Auth::attempt($request->only(['email', 'password']))) {
-    		return response()->json([
-    			'user' => Auth::user(),
-    			'token' => Auth::user()->createToken('laraspa')->accessToken,
-    		], 200);
-    	}
+        if (Auth::attempt($request->only(['email', 'password']))) {
+            return response()->json([
+                'user' => Auth::user(),
+                'token' => Auth::user()->createToken('laraspa')->accessToken,
+            ], 200);
+        }
 
-    	return response()->json([
-    		'error' => 'Unauthorized',
-    	], 401);
+        return response()->json([
+            'error' => 'Unauthorized',
+        ], 401);
     }
 }

9567bef4027c1f61cf7cc95ea8867e5c76ff30c3
Ezekiel <iamwebwiz@gmail.com>
2019-05-12 02:14:26 +0100

    First commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+    	$validator = validator($request->all(), [
+    		'name' => 'required|string|max:255',
+    		'email' => 'required|string|email|max:255',
+    		'password' => 'required|string|max:255|confirmed'
+    	]);
+
+    	if ($validator->fails()) {
+    		return response()->json([
+    			'errors' => $validator->errors(),
+    		], 401);
+    	}
+
+    	$data = $request->only(['name', 'email', 'password']);
+    	$data['password'] = bcrypt($data['password']);
+
+    	$user = User::create($data);
+
+    	return response()->json([
+    		'user' => $user,
+    		'token' => $user->createToken('laraspa')->accessToken,
+    	], 201);
+    }
+
+    public function login(Request $request)
+    {
+    	if (Auth::attempt($request->only(['email', 'password']))) {
+    		return response()->json([
+    			'user' => Auth::user(),
+    			'token' => Auth::user()->createToken('laraspa')->accessToken,
+    		], 200);
+    	}
+
+    	return response()->json([
+    		'error' => 'Unauthorized',
+    	], 401);
+    }
+}
---
+++ resources/views/landing.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<title>Laravel SPA</title>
+	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+	<script src="{{ mix('js/bootstrap.js') }}"></script>
+	<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
